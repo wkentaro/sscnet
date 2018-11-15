117733aff97c84feb2a2636604df307350c8c1fd
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-11-15 16:13:38 +0000

    Add view_RLEfile.py

---
+++ python_code/view_RLEfile.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+
+import numpy as np
+
+import utils
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('bin_file')
+    args = parser.parse_args()
+
+    with open(args.bin_file, 'rb') as f:
+        voxOriginWorld = np.frombuffer(f.read(4 * 3), dtype=np.float32)
+        camPoseArr = np.frombuffer(f.read(4 * 16), dtype=np.float32)
+
+        print('voxOriginWorld:', voxOriginWorld)
+        print('camPoseArr:', camPoseArr)
+
+        sceneVoxRLE = np.frombuffer(f.read(), dtype=np.uint32)
+        sceneVoxRLE = sceneVoxRLE.reshape(-1, 2)
+        sceneVox_values = sceneVoxRLE[:, 0]
+        sceneVox_repeats = sceneVoxRLE[:, 1]
+        voxSize = (240, 144, 240)
+        sceneVox = sceneVox_values.repeat(sceneVox_repeats).reshape(voxSize)
+
+    utils.show_volume(sceneVox)
+
+
+if __name__ == '__main__':
+    main()
